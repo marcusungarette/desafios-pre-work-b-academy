98a0c5b844a4aff89890d98924af59d46df14dca
marcusungarette <marcusungarette@gmail.com>
2021-08-17 18:01:59 -0300

    Exercicio 4 - Toogle entre o Link e o App com evento de click

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,13 @@
 import "./style.css";
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = (document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`;
+`);
+
+const link = document.querySelector('[data-js="link"]');
+
+link.addEventListener("click", () => {
+  const element = document.getElementById("app");
+  element.classList.toggle("appHidden");
+});
